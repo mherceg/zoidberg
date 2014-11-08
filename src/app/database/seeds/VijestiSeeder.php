8a1512591ba8af047e9eb228085c549d8685f1a1
Brigita Vrbanec <vrbanec.brigita@gmail.com>
2014-11-08 20:41:00 +0100

    Create VijestiSeeder so that we have some data to test with

---
+++ src/app/database/seeds/VijestiSeeder.php
@@ -0,0 +1,40 @@
+<?php
+
+class VijestiSeeder extends \Illuminate\Database\Seeder
+{
+
+    public function run()
+    {
+        DB::table('vijesti')->delete();
+
+        $this->command->info('Deleted all Vijesti');
+
+        User::where('email', '=', 'default@ministarstvo.hr')->delete();
+        $this->command->info('Deleted all users with email default@ministarstvo.hr');
+
+        $defaultAuthor = User::create(array(
+            'email' => 'default@ministarstvo.hr',
+            'password' => 'ovonitkonecepogoditi',
+            'ime' => 'Difolto',
+            'prezime' => 'Difoltich',
+            'tip' => 0,
+            'funkcija' => 'Default',
+            'd_dozvola' => 5,
+            'aktiviran' => true
+        ));
+
+        $this->command->info('Created default user');
+
+        Vijesti::create(array(
+            'autor_id' => $defaultAuthor->id,
+            'objavljeno' => true,
+            'naslov' => 'Prva vijest',
+            'sadrzaj' => '# Ljudi moji\nPotrošili smo **proračun** za 2014!',
+            'datum' => new DateTime('now')
+        ));
+
+        $this->command->info('Finished seeding vijesti');
+
+    }
+
+} 
