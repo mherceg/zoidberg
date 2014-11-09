f68f7f2122046e9c4f6e8e52c39c63d8a998a27a
Denis <denis.milosevic@gmail.com>
2014-11-09 03:45:20 +0100

    dodatci za markdown

--- src/app/views/home.blade.php
+++ src/app/views/home.blade.php
@@ -1,7 +1,18 @@
 @extends('main')
 
-<?php use Ciconia\Ciconia; 
+<?php 
+    use Ciconia\Ciconia; 
+    use Ciconia\Extension\Gfm;
+    
     $ciconia = new Ciconia();
+
+    $ciconia->addExtension(new Gfm\FencedCodeBlockExtension());
+    $ciconia->addExtension(new Gfm\TaskListExtension());
+    $ciconia->addExtension(new Gfm\InlineStyleExtension());
+    $ciconia->addExtension(new Gfm\WhiteSpaceExtension());
+    $ciconia->addExtension(new Gfm\TableExtension());
+    $ciconia->addExtension(new Gfm\UrlAutoLinkExtension());
+
 ?>
 
 @section('mainbar')
