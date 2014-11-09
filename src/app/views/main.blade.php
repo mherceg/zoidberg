d01066357620cdbe6a0d892e6477c3708a5bdf85
Brigita Vrbanec <vrbanec.brigita@gmail.com>
2014-11-09 16:50:56 +0100

    Fix relative asset URLs by using the asset() helper

--- src/app/views/main.blade.php
+++ src/app/views/main.blade.php
@@ -14,12 +14,12 @@
     <link href='http://fonts.googleapis.com/css?family=Lato:300,400,300italic,400italic' rel='stylesheet' type='text/css'>
     <link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'> 
     <!-- Global CSS -->
-    <link rel="stylesheet" href="assets/plugins/bootstrap/css/bootstrap.min.css">   
+    <link rel="stylesheet" href="{{asset('assets/plugins/bootstrap/css/bootstrap.min.css')}}">
     <!-- Plugins CSS -->
-    <link rel="stylesheet" href="assets/plugins/font-awesome/css/font-awesome.css">
+    <link rel="stylesheet" href="{{asset('assets/plugins/font-awesome/css/font-awesome.css')}}">
     
     <!-- Theme CSS -->  
-    <link id="theme-style" rel="stylesheet" href="assets/css/styles.css">
+    <link id="theme-style" rel="stylesheet" href="{{asset('assets/css/styles.css')}}">
     <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
@@ -32,7 +32,7 @@
     <!-- ******HEADER****** --> 
     <header class="header">
         <div class="container">                       
-            <img class="profile-image img-responsive pull-left" src="assets/images/zoidberg_grb.png" width="150" height="150" />
+            <img class="profile-image img-responsive pull-left" src="{{asset('assets/images/zoidberg_grb.png" width="150" height="150')}}" />
             <div class="profile-content">
                 <h1 class="name">{{$ministarstvo}}</h1>
                 <h2 class="desc">{{$podnaziv}}</h2>   
@@ -76,12 +76,12 @@
     </footer><!--//footer-->
  
     <!-- Javascript -->          
-    <script type="text/javascript" src="assets/plugins/jquery-1.11.1.min.js"></script>
-    <script type="text/javascript" src="assets/plugins/jquery-migrate-1.2.1.min.js"></script>
-    <script type="text/javascript" src="assets/plugins/bootstrap/js/bootstrap.min.js"></script>    
-    <script type="text/javascript" src="assets/plugins/jquery-rss/dist/jquery.rss.min.js"></script> 
+    <script type="text/javascript" src="{{asset('assets/plugins/jquery-1.11.1.min.js')}}"></script>
+    <script type="text/javascript" src="{{asset('assets/plugins/jquery-migrate-1.2.1.min.js')}}"></script>
+    <script type="text/javascript" src="{{asset('assets/plugins/bootstrap/js/bootstrap.min.js')}}"></script>    
+    <script type="text/javascript" src="{{asset('assets/plugins/jquery-rss/dist/jquery.rss.min.js')}}"></script> 
     <!-- custom js -->
-    <script type="text/javascript" src="assets/js/main.js"></script>            
+    <script type="text/javascript" src="{{asset('assets/js/main.js')}}"></script>            
 </body>
 </html> 
 
