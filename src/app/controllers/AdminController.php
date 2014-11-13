276d4e33ca00c576ef386c4858680abc9a45dffa
Denis <denis.milosevic@gmail.com>
2014-11-13 11:45:58 +0100

    Uredivanje osnovnih podataka

---
+++ src/app/controllers/AdminController.php
@@ -0,0 +1,51 @@
+<?php
+
+class AdminController extends BaseController {
+
+	private $passed_data = array();
+
+    public function retrivePageTitle() {
+        return "Home";
+    }
+
+	public function getDashboard()
+	{
+		return View::make('admin.dashboard');
+	}
+
+	public function getOsnovnipodaci()
+	{
+		View::share(array(
+			'op' => OsnovniPodaci::first()
+		));
+		return View::make('admin.osnovne_informacije');
+	}
+
+	public function postOsnovnipodaci()
+	{
+		$ulaz = Input::all();
+		//var_dump($a);
+
+		$podaci = OsnovniPodaci::first();
+		$podaci->naziv = $ulaz['naziv'];
+		$podaci->podnaziv = $ulaz['podnaziv'];
+		$podaci->adresa = $ulaz['adresa'];
+		$podaci->telefon = $ulaz['telefon'];
+		$podaci->email = $ulaz['email'];
+		$podaci->emblem_lokacija = $ulaz['lemb'];
+		$podaci->slika_lokacija = $ulaz['lzg'];
+
+		$podaci->save();
+
+		View::share(array(
+			'op' => $podaci,
+			'poruka' => "Promjene su uspješno spremljene!"
+		));
+		return View::make('admin.osnovne_informacije');
+	}
+
+	public function postIndex() {
+		return $this->getIndex();
+	}
+
+}
